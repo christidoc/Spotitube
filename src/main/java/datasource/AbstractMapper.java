492108105d3f4c799eceac9e436c10cef6040d6e
Unknown <christiaantenvoorde@hotmail.com>
2018-04-02 14:56:51 +0200

    idk

--- src/main/java/datasource/AbstractMapper.java
+++ src/main/java/datasource/AbstractMapper.java
@@ -4,25 +4,84 @@ import domain.DomainObject;
 
 
 import java.sql.*;
+import java.util.List;
 
 abstract class AbstractMapper {
     MySQLConnector mySQLConnector = new MySQLConnector();
     protected abstract String findStatement();
+    protected abstract String findAllStatement();
+    protected abstract String insertStatement();
+    protected abstract String deleteStatement();
+    protected abstract String updateStatement();
+
     protected abstract DomainObject load(ResultSet rs) throws SQLException;
+    protected abstract List<DomainObject> loadAll(ResultSet rs) throws  SQLException;
+    protected abstract void doInsert(DomainObject subject, PreparedStatement insertStatement) throws SQLException;
+    protected abstract void doDelete(DomainObject subject, PreparedStatement insertStatement) throws SQLException;
+    protected abstract void doUpdate(DomainObject subject, PreparedStatement insertStatement) throws SQLException;
 
     protected DomainObject abstractFind(int id) {
         Connection DB = mySQLConnector.getConnection();
-        PreparedStatement findStatement = null;
+        PreparedStatement findStatement;
         try {
             findStatement = DB.prepareStatement(findStatement());
             findStatement.setLong(1, id);
             ResultSet rs = findStatement.executeQuery();
             rs.next();
-            DomainObject result = load(rs);
-            return result;
+            return load(rs);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public List<DomainObject> abstractFindAll() {
+        Connection DB = mySQLConnector.getConnection();
+        PreparedStatement findStatement;
+        try {
+            findStatement = DB.prepareStatement(findAllStatement());
+            ResultSet rs = findStatement.executeQuery();
+            rs.next();
+            return loadAll(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void insert(DomainObject subject) {
+        PreparedStatement insertStatement;
+        Connection DB = mySQLConnector.getConnection();
+        try {
+            insertStatement = DB.prepareStatement(insertStatement());
+            doInsert(subject, insertStatement);
+            insertStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(DomainObject subject) {
+        Connection DB = mySQLConnector.getConnection();
+        PreparedStatement deleteStatement;
+        try {
+            deleteStatement = DB.prepareStatement(deleteStatement());
+            doDelete(subject, deleteStatement);
+            deleteStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(DomainObject subject) {
+        Connection DB = mySQLConnector.getConnection();
+        PreparedStatement updateStatement;
+        try {
+            updateStatement = DB.prepareStatement(updateStatement());
+            doUpdate(subject, updateStatement);
+            updateStatement.execute();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/datasource/PlaylistMapper.java
+++ src/main/java/datasource/PlaylistMapper.java
@@ -4,6 +4,7 @@ import domain.DomainObject;
 import domain.Playlist;
 import domain.User;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -16,6 +17,24 @@ public class PlaylistMapper extends AbstractMapper implements PlaylistDAO{
         return "SELECT * FROM playlist JOIN user ON playlist.owner = user.username WHERE id= ?";
     }
 
+    protected  final String findAllStatement() {
+        return "SELECT * FROM playlist";
+    }
+
+    protected final String insertStatement() {
+        return "INSERT INTO playlist VALUES (?, ?)";
+    }
+
+    protected final String deleteStatement() {
+
+    }
+
+    public PlaylistMapper() {
+        if(playlists == null){
+            playlists = new ArrayList<>();
+        }
+    }
+
     protected DomainObject load(ResultSet rs) throws SQLException {
         int id = rs.getInt("id");
         for(Playlist playlist : playlists){
@@ -26,25 +45,49 @@ public class PlaylistMapper extends AbstractMapper implements PlaylistDAO{
         Playlist playlist = new Playlist();
         playlist.setId(id);
         playlist.setName(rs.getString("name"));
-        playlist.setOwner(new User(rs.getString("owner"), rs.getString("password")));
+        playlists.add(playlist);
         return playlist;
     }
 
-    public PlaylistMapper() {
-        if(playlists == null){
-            playlists = new ArrayList<>();
+    protected List<DomainObject> loadAll (ResultSet rs) throws SQLException {
+        List<DomainObject> playlists = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                Playlist playlist = new Playlist();
+                playlist.setId(rs.getInt("id"));
+                playlist.setName(rs.getString("name"));
+                //playlist.setOwner(new User(rs.getString("owner"), rs.getString("password")));
+                playlists.add(playlist);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
+        return playlists;
+    }
+
+    protected void doInsert (DomainObject subject, PreparedStatement insertStatement) throws SQLException {
+            Playlist playlist = (Playlist) subject;
+            insertStatement.setString(1, playlist.getName());
+            insertStatement.setString(2, playlist.getOwner().getUserName());
+            playlists.add(playlist);
     }
 
     @Override
     public List<Playlist> getAllPlaylists() {
-        //Update playlists
-        return playlists;
+        List<Playlist> returnList = new ArrayList<>();
+        for(Object o : abstractFindAll()){
+            try{
+                returnList.add((Playlist) o);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return returnList;
     }
 
     @Override
     public void addPlaylist(Playlist playlist) {
-
+        insert(playlist);
     }
 
     @Override
@@ -67,6 +110,5 @@ public class PlaylistMapper extends AbstractMapper implements PlaylistDAO{
             }
         }
         return (Playlist) abstractFind(id);
-        //check database for playlist and add playlist to playlists if found.
     }
 }
